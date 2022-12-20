7f61b5c0dddf962d4ac57dc405819b3cde3f0af7
Kailash Chandrasekaran <ckailash@users.noreply.github.com>
2022-12-20 21:12:48 +1100

    Remove `dev` as a network option

--- deploy/utils.ts
+++ deploy/utils.ts
@@ -1,7 +1,5 @@
 export function getIMXAddress(network: string) {
     switch (network) {
-        case 'dev':
-            return '0x3e6e01355bB66925a65D372bf9c9f3835d9964fA';
         case 'sandbox':
             return '0x7917eDb51ecD6CdB3F9854c3cc593F33de10c623';
         case 'mainnet':
